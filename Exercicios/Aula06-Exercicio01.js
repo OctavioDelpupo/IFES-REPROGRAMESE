bcbf3a6ab4c9ec7865ab51b8549b9f594f4a44cf
Octavio Delpupo <octavio.delpupo@gmail.com>
2023-09-25 15:24:20 -0300

    Exercicio 06.

---
+++ Exercicios/Aula06-Exercicio01.js
@@ -0,0 +1,9 @@
+prompt = require("prompt-sync")();
+
+salario = parseFloat(prompt("Digite o salario: "))
+ir = parseFloat(prompt("Digite IR: "))
+
+salarioLiquido = salario - ir
+
+console.log("Salário liquido é: ", salarioLiquido)
+
