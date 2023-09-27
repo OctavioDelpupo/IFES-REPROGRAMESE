cfe2cdbe15c174901528124221220fa0b966a244
Octavio Delpupo <octavio.delpupo@gmail.com>
2023-09-26 21:46:15 -0300

    Exercício 03 Aula 7 comandos de decisão.

--- Exercicios/Aula07-Exercicio03.js
+++ Exercicios/Aula07-Exercicio03.js
@@ -3,12 +3,12 @@ prompt = require("prompt-sync")();
 var nomeCliente = String(prompt("Nome do Cliente: "));
 var varloDaCompra = parseFloat(prompt("Valor da compra R$: "));
 
-if (varloDaCompra > 1000) {
-  var desconto = varloDaCompra / 0.5 - varloDaCompra;
+if (varloDaCompra < 1000) {
+  var desconto = varloDaCompra * 0.05;
 } else if (varloDaCompra < 5000) {
-  var desconto = varloDaCompra - varloDaCompra / 0.1;
+  var desconto = varloDaCompra * 0.1;
 } else {
-  var desconto = varloDaCompra - varloDaCompra / 0.15;
+  var desconto = varloDaCompra * 0.15;
 }
 
-console.log("Nome : ", nomeCliente, "\nDesconto : ", desconto);
+console.log("\nNome:", nomeCliente, "\nDesconto:", desconto);
